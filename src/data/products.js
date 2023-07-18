fc20a36867f29cb05677aac095a4ea797654d274
Stalin Acurio <sta_vla411@hotmail.com>
2023-07-18 00:26:44 -0500

    --- change to js file

---
+++ src/data/products.js
@@ -0,0 +1,162 @@
+export const products = [
+  {
+    id: 1,
+    name: "Sable de luz de Anakin Skywalker",
+    price: 59.99,
+    description:
+      "Réplica del sable de luz usado por Anakin Skywalker antes de convertirse en Darth Vader.",
+    category: "Star Wars",
+    image: "/6.webp",
+  },
+  {
+    id: 2,
+    name: "Figura de acción de Rey",
+    price: 29.99,
+    description:
+      "Figura de acción detallada de Rey, la heroína de la trilogía más reciente de Star Wars.",
+    category: "Star Wars",
+    image: "/9.webp",
+  },
+  {
+    id: 3,
+    name: "Nave espacial Millennium Falcon",
+    price: 129.99,
+    description:
+      "Modelo de la famosa nave espacial Millennium Falcon, pilotada por Han Solo y Chewbacca.",
+    category: "Star Wars",
+    image: "/12.webp",
+  },
+  {
+    id: 4,
+    name: "Peluche de Baby Yoda",
+    price: 19.99,
+    description:
+      "Adorable peluche de Baby Yoda, también conocido como Grogu, de la serie 'The Mandalorian'.",
+    category: "Star Wars",
+    image: "/15.webp",
+  },
+  {
+    id: 5,
+    name: "Juego de mesa de Star Wars: Rebellion",
+    price: 49.99,
+    description:
+      "Juego de estrategia donde los jugadores pueden controlar la Alianza Rebelde o el Imperio Galáctico.",
+    category: "Juegos",
+    image: "/16.webp",
+  },
+  {
+    id: 6,
+    name: "Camiseta de la Estrella de la Muerte",
+    price: 14.99,
+    description:
+      "Camiseta con el diseño de la Estrella de la Muerte, la estación espacial del Imperio.",
+    category: "Camisetas",
+    image: "/17.webp",
+  },
+  {
+    id: 7,
+    name: "PlayStation 5",
+    price: 499.99,
+    description:
+      "La última consola de Sony con gráficos de alta calidad y compatibilidad con juegos de última generación.",
+    category: "Consolas",
+    image: "/1.webp",
+  },
+  {
+    id: 8,
+    name: "Xbox Series X",
+    price: 499.99,
+    description:
+      "La consola más potente de Microsoft con capacidad de reproducción en 4K y una amplia biblioteca de juegos.",
+    category: "Consolas",
+    image: "/2.webp",
+  },
+  {
+    id: 9,
+    name: "Nintendo Switch",
+    price: 299.99,
+    description:
+      "Consola híbrida que te permite jugar en casa o en cualquier lugar gracias a su diseño portátil.",
+    category: "Consolas",
+    image: "/3.webp",
+  },
+  {
+    id: 10,
+    name: "PlayStation 4",
+    price: 299.99,
+    description:
+      "Consola anterior de Sony con una amplia selección de juegos y entretenimiento multimedia.",
+    category: "Consolas",
+    image: "/8.webp",
+  },
+  {
+    id: 11,
+    name: "Xbox Series S",
+    price: 299.99,
+    description:
+      "Versión más económica de la Xbox Series X, ideal para juegos en resolución 1080p.",
+    category: "Consolas",
+    image: "/13.webp",
+  },
+  {
+    id: 12,
+    name: "Nintendo Switch Lite",
+    price: 199.99,
+    description:
+      "Versión más compacta y portátil de la Nintendo Switch, diseñada exclusivamente para juegos en modo portátil.",
+    category: "Consolas",
+    image: "/10.webp",
+  },
+  {
+    id: 13,
+    name: "Camiseta de Star Wars",
+    price: 19.99,
+    description:
+      "Camiseta con estampado del logotipo de Star Wars en color negro.",
+    category: "Camisetas",
+    image: "/4.webp",
+  },
+  {
+    id: 14,
+    name: "Juego de mesa de Star Wars",
+    price: 29.99,
+    description:
+      "Juego de estrategia basado en el universo de Star Wars con miniaturas y cartas.",
+    category: "Juegos",
+    image: "/5.webp",
+  },
+  {
+    id: 15,
+    name: "Figura coleccionable de Yoda",
+    price: 39.99,
+    description: "Figura detallada de Yoda con acabado pintado a mano.",
+    category: "Muñecos",
+    image: "/6.webp",
+  },
+  {
+    id: 16,
+    name: "Llavero de Millennium Falcon",
+    price: 9.99,
+    description:
+      "Llavero metálico con forma de la nave espacial Millennium Falcon.",
+    category: "Accesorios",
+    image: "/7.webp",
+  },
+  {
+    id: 17,
+    name: "Taza de BB-8",
+    price: 14.99,
+    description: "Taza con diseño de BB-8, el simpático droide de Star Wars.",
+    category: "Vajilla",
+    image: "/11.webp",
+  },
+  {
+    id: 18,
+    name: "Libro de arte de Star Wars",
+    price: 49.99,
+    description:
+      "Libro de tapa dura con ilustraciones y arte conceptual de las películas de Star Wars.",
+    category: "Libros",
+    image: "/18.webp",
+  },
+];
