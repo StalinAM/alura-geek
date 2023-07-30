dae37294f927de8f05ae8adc3b7dca9b3626a84f
Stalin <sta_vla411@hotmail.com>
2023-07-30 18:21:10 -0500

    add deploy in github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://github.com/StalinAM",
+  base: "/alura-geek",
   integrations: [tailwind()],
   output: "server",
 });
